3dda9655eb78e73d63c48414727b292709348f47
EFG <efg@efg.local>
2025-09-15 21:10:54 -0300

    commit inicial

---
+++ sistema_distribuido/tarefa_distribuida.py
@@ -0,0 +1,24 @@
+#pip install paramiko
+import paramiko
+
+nodes = {
+    'master-node': 'master',
+    'worker1-node': 'worker1',
+    'worker2-node': 'worker2'
+    }
+
+def  executar_tarefa_no_node(container_name):
+    #simular uma conexão SSH para execultar uma tarefa em um nó
+    print(f" *** Conectando a {container_name}")
+    #chamar as funções de conexão
+    #cliente = paramiko.SSHClient()
+    #cliente.connect(container_name, username='root', password='admin')
+    #confirmar execução
+    print("Tarefa execultada com sucesso em {container_name}")
+
+def distribuir_tarefa():
+    #Distribuir uma tarefa simples para todos os nós do cluster
+    for container_name in nodes:
+        executar_tarefa_no_node(container_name)
+
+distribuir_tarefa()
