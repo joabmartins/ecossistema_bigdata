16a1216cf28c47051c65717b54df1a6e640dff8c
josephfaustinobezerra <josephfaustinobsb@gmail.com>
2025-09-23 20:54:48 -0300

    código apache spark

---
+++ sistema_replicado/euro_srv/app_eu.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Carrega o arquivo CSV I
+df = pd.read_csv('/data /dados.csv')
+
+# imprime o DataFrame para confirmar
+print("--- Dados lidos com sucesso na Europa!")
+print(df)
