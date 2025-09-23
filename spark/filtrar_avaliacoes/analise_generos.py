ab597cf6c5486c193586057a5b949fa325f71de2
EFG <efg@efg.local>
2025-09-23 20:54:50 -0300

    código apache spark

---
+++ spark/filtrar_avaliacoes/analise_generos.py
@@ -0,0 +1,38 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split, count
+
+# 1. Inicia a sessão Spark
+spark = SparkSession.builder.appName("AnaliseGenerosPopulares").getOrCreate()
+
+# 2. Lê o arquivo csv
+df = spark.read.csv("filmes.csv", header=True, inferSchema=True)
+
+# 3. obtem filmes com nota acima 4
+df_altas_avaliacoes = df.filter(df["Avaliacao"] > 4.0)
+
+# 4. Divide a coluna genero
+df_generos_array = df_altas_avaliacoes.withColumn(
+    "genero_array",
+    split(df_altas_avaliacoes.genero, ";")
+)
+
+# 5. explode (separa) o array de generos diferentes
+df_generos_explode = df_generos_array.withColumn(
+    "genero_individual",
+    explode(df_generos_array.genero_array)
+ )
+
+# 6. Agrupa os dados por cada genero e conta a frequencia
+df_resultado = df_generos_explode.groupBy("genero_individual") \
+.agg(count("*").alias("total_filmes")) \
+.orderBy("total_filmes", ascending=False)
+
+# 7. Exibir os dados
+print(df_resultado.show(10, truncate=False))
+
+# 8. Fecha a sessão
+spark.stop()
+
+#docker-compose up
+
+
